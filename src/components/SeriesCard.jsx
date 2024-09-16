36712ae5732bf9a1124f538cbcc8e245086f7310
ishagaikwad07 <iamishagaikwad07@gmail.com>
2024-09-16 18:40:35 +0530

    Added CSS Inline Styling

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,20 +1,29 @@
 export const SeriesCard = (props)=>{
   // console.log(props);
   const {id,img_url,name,rating,description,cast,genre,watch_url} =props.data;
+
+  const btn_style={
+    padding:"1.2rem 2.4rem",
+    border: "none",
+    fontSize:"1.6rem",
+    backgroundColor:"var(--bnt-hover-bg-color)",
+    color:"var(--bg-color)"
+  }
     return (
-        <li>
+        <li className="card">
           <div>
-          <img  width = "50%" height = "70%" src={img_url} alt={name} />
+          <img  width = "40%" height = "40%" src={img_url} alt={name} />
             </div>
+            <div className="card-content">
           <h2>Name: {name}</h2>
-          <h3>Rating: {rating}</h3>
-          <p>Summary:{description} </p>
-          <p>Genre: {genre}</p>
-          <p>Cast:{cast}</p>
+          <h3 >Rating: {rating}</h3>
+          <p >Summary:{description} </p>
+          <p >Genre: {genre.join("")}</p>
+          <p >Cast:{cast.join("")}</p>
           <a href={watch_url} target ="_blank" rel="noopener noreferrer">
-          <button>Watch Now</button>
+          <button style={btn_style}>Watch Now</button>
           </a>
-      
+          </div>
         </li>
     );
 };
