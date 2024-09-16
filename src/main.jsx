474b425bfcdd931b98dbfe21ec2848489021eec0
ishagaikwad07 <iamishagaikwad07@gmail.com>
2024-09-16 16:28:10 +0530

    JSX as Props

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App"; // Ensure the correct path to your App file
-import { Practices } from "./practices";
+// import { App } from "./App"; // Ensure the correct path to your App file
+// import { Practices } from "./practices";
+import Profile from "./components/Profile";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
     {/* <Practices/> */}
+    <Profile/>
   </React.StrictMode>,
   document.getElementById("root") // Ensure there's a root element in your HTML
 );
